10c0d7c7cbe348ec08142ed46a6793c1bd4b81d1
DiegoNava10 <151683046+DiegoNava10@users.noreply.github.com>
2024-06-22 17:36:13 -0600

    Formulario Reactivo contactanos/correo
    Se realizo un formulario reactivo en contactanos/correo

--- placoAngular/src/app/correo/correo.component.ts
+++ placoAngular/src/app/correo/correo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { FormControl,FormGroup,ReactiveFormsModule,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-correo',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './correo.component.html',
   styleUrl: './correo.component.css'
 })
@@ -18,9 +19,23 @@ export class CorreoComponent {
   telefono: string = '';
   mensaje: string = '';
 
+  resultado!: string;
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    apellido: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    mensaje: new FormControl('', [Validators.required, Validators.maxLength(500)]),
+    telefono: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")])
+  });
+
   constructor(private http: HttpClient) {}
 
   onSubmit() {
+    if(this.formularioContacto.valid)
+      this.resultado = "Todos los datos son validos";
+    else
+      this.resultado = "Hay datos invalidos en el formulario";
     const cuerpoCorreo = `        
         Hola ${this.nombre} ${this.apellido},
         Gracias por tu mensaje:\n
